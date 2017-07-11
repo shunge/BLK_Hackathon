b7f7f25904278d16c7f11f53904bcde6b2fd786a
Yiming Wei <yimingwei@Yimings-MacBook-Pro.local>
2017-07-11 14:31:24 -0700

    wraped the pdf2text part

--- test_main.py
+++ test_main.py
@@ -1,3 +1,14 @@
-from pdf2txt import *
+import pdf2txt
 from RequestInterface import *
+
+
+def main():
+    worker = pdf2txt.ConvertToText("samples/", "samples/")
+    worker.parse("sample-resumes_scs_1.pdf", "sample-resumes_scs_1.txt")
+
+
+
+if __name__ == '__main__':
+    main()
+
 
