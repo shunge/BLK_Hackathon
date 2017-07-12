64a35a19719629a473db5e7ad617a1bbd72c5909
Yishun Guan <gyishun@gmail.com>
2017-07-12 09:14:46 -0700

    Added major extraction

--- RequestInterface.py
+++ RequestInterface.py
@@ -1,6 +1,8 @@
 # importing the requests library
 import requests
 import json
+import csv
+from sets import Set
 
 def ReadTextFiles(fileName):
     with open(fileName, "r") as myfile:
@@ -25,8 +27,26 @@ def SearchEntity(ResultJSON, entity):
     length = len(ResultJSON[u'entities'])
     for i in range(length):
         entry = ResultJSON[u'entities'][i]
-        if entity == entry[u'type']:
-            print entry[u'name']
-    #print ResultJSON[u'entities']
+
+def ParseCSVandSaveMajors():
+    return;
+
+def File_Reader(file_obj):
+    All_Majors = []
+    for line in f_obj:
+        #print line
+        All_Majors.append(line.rstrip())
+    return All_Majors
 
 SearchEntity(GetEntityResult(ReadTextFiles("samples/output.txt")),"PERSON")
+All_Majors = []
+with open("majors.txt") as f_obj:
+    All_Majors = File_Reader(f_obj)
+
+
+for major in All_Majors:
+    #print major.lower()
+    str = major.lower()
+    searchfile = open("samples/output_13.txt", "r")
+    for line in searchfile:
+        if major.lower() in line.lower(): print major.lower()
