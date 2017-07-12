fd0f323cee37e340195278ab6f7506cdcaad3a5e
Chris <christopher.cahill@aol.com>
2017-07-12 16:00:53 -0700

    adding solr uploader

---
+++ solr_uploader.py
@@ -0,0 +1,34 @@
+import solr
+import csv
+import re
+
+# create a connection to a solr server
+s = solr.Solr('http://localhost:8983/solr/resume_core')
+
+with open('parsedResume.csv', 'rb') as csvfile:
+     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+     for row in spamreader:
+         id = int(row[0])
+         pdf_path = "static/" + row[1]
+         text_path = row[1] + ".txt"
+         text_file = open(text_path, 'r')
+         text = text_file.read().decode('utf-8', 'ignore')
+         text = re.sub(r'\W+', ' ', text)
+         print(id)
+         print(pdf_path)
+         print(text_path)
+         # doc = dict(
+         #     id=id,
+         #     pdf_path=pdf_path,
+         #     resume_text=text,
+         # )
+         # s.add(doc, commit=True)
+
+
+
+
+
+
+
+
+
