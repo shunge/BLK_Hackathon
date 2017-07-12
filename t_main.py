dad231178e2947bdc99a757fe6a441842203c3e6
Yishun Guan <gyishun@gmail.com>
2017-07-12 16:37:54 -0700

    Fixed Codes

--- t_main.py
+++ t_main.py
@@ -19,13 +19,6 @@ def helper(argv):
 if __name__ == '__main__':
 
     list = []
-    # for i in range (1, 14):
-    #     path = "samples/sample-resumes_scs_%d.pdf" % i
-    #     list.append(helper(["pdf2txt.py", path]))
-    #
-    # for i in range(1, 3):
-    #     path = "samples/sample-resumes_finance_%d.pdf" % i
-    #     list.append(helper(["pdf2txt.py", path]))
 
     for filename in glob.glob('samples/*.pdf'):
         print filename
