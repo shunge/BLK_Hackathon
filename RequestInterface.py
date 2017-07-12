af78c9196e5e9da7ba37a8d3d9eba786b47603c9
Yishun Guan <gyishun@gmail.com>
2017-07-12 10:36:37 -0700

    Added University Extraction

--- RequestInterface.py
+++ RequestInterface.py
@@ -2,7 +2,8 @@
 import requests
 import json
 import csv
-from sets import Set
+import re
+import string
 
 def ReadTextFiles(fileName):
     with open(fileName, "r") as myfile:
@@ -43,10 +44,28 @@ All_Majors = []
 with open("majors.txt") as f_obj:
     All_Majors = File_Reader(f_obj)
 
+def Major_Extraction(path):
+    for major in All_Majors:
+        major_lower = major.lower()
+        searchfile = open(path, "r")
+        for line in searchfile:
+            Searchable_line = line.lower().replace("-", " ")
+            if major_lower in Searchable_line:
+                if re.search('(masters|bachelor|b.s.|bs|major|phd|ms)', Searchable_line) is not None:
+                    print major_lower
 
-for major in All_Majors:
-    #print major.lower()
-    str = major.lower()
-    searchfile = open("samples/output_13.txt", "r")
-    for line in searchfile:
-        if major.lower() in line.lower(): print major.lower()
+All_Schools = []
+with open("Uni.txt") as f_obj:
+    All_Schools = File_Reader(f_obj)
+
+def School_Extraction(path):
+    for school in All_Schools:
+        school_lower = school.lower()
+        searchfile = open(path, "r")
+        for line in searchfile:
+            Searchable_line = line.lower()
+            if school_lower in Searchable_line:
+                    print school_lower
+
+Major_Extraction("samples/output_13.txt")
+School_Extraction("samples/output_13.txt")
